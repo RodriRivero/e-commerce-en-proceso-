43482830edf15ceb429506f5cc2c5ca793df274b
rodrigoss88 <guidorodrigorivero@outlook.com>
2022-05-24 23:04:48 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React11
-        </a>
-      </header>
-    </div>
-  );
+  return <div> 
+    <h1>reacts223</h1>
+  
+  <h2>Rodrigo Rivero</h2>
+  
+  </div>
+  
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+
+function NavBar() {
+    return <h1>componente NavBar</h1>;
+    
+    }
+
+    export default NavBar;
